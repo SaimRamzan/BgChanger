3f767f2329cafdb81f32137d4a92c05fd4bed4b4
SaimRamzan <Saimshinwair001@gmail.com>
2024-03-08 12:42:55 +0500

    Complete Project

---
+++ BgChanger/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setcolor] = useState("grey");
+  return (
+    <div className="main-div " style={{ backgroundColor: color }}>
+      <div className="box">
+        <div className="childbox">
+          <button
+            style={{ backgroundColor: "Red" }}
+            onClick={() => setcolor("red")}
+          >
+            Red
+          </button>
+          <button
+            style={{ backgroundColor: "Green" }}
+            onClick={() => setcolor("Green")}
+          >
+            Green
+          </button>
+          <button
+            style={{ backgroundColor: "BLUE" }}
+            onClick={() => setcolor("Blue")}
+          >
+            Blue
+          </button>
+          <button
+            style={{ backgroundColor: "grey" }}
+            onClick={() => setcolor("Grey")}
+          >
+            Grey
+          </button>
+          <button
+            style={{ backgroundColor: "pink" }}
+            onClick={() => setcolor("pink")}
+          >
+            Pink
+          </button>
+          <button
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => setcolor("yellow")}
+          >
+            Yellow
+          </button>
+          <button
+            style={{ backgroundColor: "Purple" }}
+            onClick={() => setcolor("Purple")}
+          >
+            Purple
+          </button>
+          <button
+            style={{ backgroundColor: "Black" }}
+            onClick={() => setcolor("Black")}
+          >
+            Black
+          </button>
+          <button
+            style={{ backgroundColor: "Olive" }}
+            onClick={() => setcolor("Olive")}
+          >
+            Olive
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
